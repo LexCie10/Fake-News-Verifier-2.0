2d5b37837c9b38a169761ae383b14a1e3530f44d
LexCie10 <alexanderlawrence70@gmail.com>
2025-02-21 11:07:01 +0100

    Update FNVapp.py

--- FNVapp.py
+++ FNVapp.py
@@ -64,7 +64,7 @@ st.markdown("<h1 style='text-align: center; color: #4A7DF5;'>FAKE NEWS VERIFIER 
 st.write("<h3 style='text-align: center;'>Enter news article title, content and source URL(Optional) here to verify if it's <b>Fake</b> or <b>Real</b>.</h3>", unsafe_allow_html=True)
 
 # input fields for getting user values for X (title and text fields)
-news_url = st.text_input("🔗 Enter the news source URL(Optional)")
+news_url = st.text_input("🔗URL", placeholder="Enter the news source URL(Optional)")
 title = st.text_input("Title", placeholder="Enter the news headline here...")
 text = st.text_area("Content", placeholder="Paste the full news article here...")
 
